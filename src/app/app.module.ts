6eac147b5dde1bf8ac4c9e42a0ff8bf9a45768f4
Ryan Gordon <ryanmg@bgsu.edu>
2019-03-15 13:59:25 -0400

    Separated heroes component into second component; hero-details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';   
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
